8c1c30b8fb7ff9d335b760585ecc962d5184ec4b
Fidia-e <elbouanani.f@gmail.com>
2024-02-24 18:06:00 +0100

    maj

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div>
+			<h1>
+				Enfant d&apos;chienne de torvisse d&apos;ostifie de cibouleau de bâtard de cibole de colon d&apos;astie de
+				ciboire de câlique de bout d&apos;crisse de crucifix de doux Jésus de maudine de saintes fesses de calvince de
+				verrat de viande à chien de saint-cimonaque de sacristi de boswell de marde de baptême de gériboire de mosus.
+			</h1>
+		</div>
+	);
+};
+
+export default Home;
