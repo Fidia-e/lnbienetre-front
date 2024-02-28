6d2d2a644ddf2cf0fe0dc47de7ddd6b593568cd1
Fidia-e <elbouanani.f@gmail.com>
2024-02-28 18:22:15 +0100

    legales page integration

--- src/pages/LegalesMentions/index.tsx
+++ src/pages/LegalesMentions/index.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const LegalesMentions = () => {
 	return (
 		<div className="legales-container">
-			<p>Mentions légales</p>
+			<h1 className="title">Mentions légales</h1>
+			<p className="legales-text">Les mentions légales son en cours de rédaction ! :)</p>
 		</div>
 	);
 };
